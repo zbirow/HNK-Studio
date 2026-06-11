444fbebc72af8fba3e3dc2414f8c437a6fd60624
zbirow <77003019+zbirow@users.noreply.github.com>
2026-06-11 19:56:53 +0200

    Add options to exportAsset and new import/save functions

--- src/main/preload.cjs
+++ src/main/preload.cjs
@@ -10,7 +10,12 @@ contextBridge.exposeInMainWorld("hnkApi", {
   },
   openFile: (gameId) => ipcRenderer.invoke("hnk:open", { gameId }),
   previewAsset: (nodeId, options = {}) => ipcRenderer.invoke("asset:preview", { nodeId, ...options }),
-  exportAsset: (nodeId, format) => ipcRenderer.invoke("asset:export", { nodeId, format }),
+  exportAsset: (nodeId, format, options = {}) => ipcRenderer.invoke("asset:export", { nodeId, format, ...options }),
   exportAll: (nodeId, format) => ipcRenderer.invoke("asset:exportAll", { nodeId, format }),
+  importDat: (nodeId, data) => ipcRenderer.invoke("asset:importDat", { nodeId, data }),
+  importTextureDds: (nodeId, dds) => ipcRenderer.invoke("texture:importDds", { nodeId, dds }),
+  saveTextureEdit: (nodeId, edit) => ipcRenderer.invoke("texture:saveEdit", { nodeId, ...edit }),
+  saveSpriteTable: (nodeId, sprites) => ipcRenderer.invoke("sprite:saveTable", { nodeId, sprites }),
+  saveSpriteFontCharacters: (nodeId, characters) => ipcRenderer.invoke("sprite:saveFontCharacters", { nodeId, characters }),
   selectSoundsFolder: () => ipcRenderer.invoke("sounds:selectFolder")
 });
